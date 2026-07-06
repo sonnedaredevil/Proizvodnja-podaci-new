8c0fa0349b50bc477075bed4bc9728a76e125ec7
sonnedaredevil <sonnedaredevil@gmail.com>
2026-07-06 17:16:17 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+[theme]
+base = "dark"
+primaryColor = "#38BDF8"
+backgroundColor = "#07111F"
+secondaryBackgroundColor = "#0F172A"
+textColor = "#F8FAFC"
+font = "sans serif"
+
+[client]
+toolbarMode = "minimal"
